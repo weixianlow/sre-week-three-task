fb8b9223301145b8017e5da394ecef87f6c5d9a3
Wei Xian Low <email@weixianlow.me>
2024-04-20 15:53:45 +0000

    Task 1 and 2 Completion

---
+++ README.md
@@ -0,0 +1,11 @@
+To be able to address the toll in the ticketing system, an alerting system for SRE must be made available to be able to react to events and alert happening in real time. 
+
+Multiple solution from either open-source or commercial products is available as follows: 
+
+### Open-Source Solutions
+1. OpenDuty
+2. Grafana Alerting
+
+### Commercial Product Solutions
+1. PagerDuty
+2. Splunk On-Call
--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,21 @@
 #TODO
+
+NAMESPACE="sre"
+
+DEPLOYMENT="swype-app"
+
+MAX_RESTARTS=3
+
+while true; do
+    RESTARTS=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+
+    echo "Current number of restarts: ${RESTARTS}"
+
+    if ((RESTARTS > MAX_RESTARTS )); then
+        echo "Maximum number of restarts exceeded. Will now scaling down the deployment."
+        kubectl scale --replicas=0 deployment/${DEPLOYMENT} -n ${NAMESPACE}
+        break
+    fi
+
+    sleep 60
+done
